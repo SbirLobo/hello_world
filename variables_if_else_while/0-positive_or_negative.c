eb486fd499acab4d5ac9bed819390120fdf50a3f
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-17 12:32:28 +0000

    0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'description for betty requirement'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+		{
+		if (n > 0)
+		{
+			printf("%d is positive\n", n);
+		}
+		else
+		{
+			printf("%d is zero\n", n);
+		}
+		}
+	return (0);
+
+}

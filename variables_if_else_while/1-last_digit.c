3e290907f50cf59b2f4b7481d90af3aea951569f
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-17 13:35:28 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -25,17 +25,17 @@ int main(void)
 
 	if (last_digit == 0)
 	{
-		printf("is 0\n");
+		printf("0\n");
 	}
 	else
 		{
 		if (last_digit < 6)
 		{
-			printf("is less than 6 and not 0\n");
+			printf("less than 6 and not 0\n");
 		}
 		else
 		{
-			printf("is greater than 5\n");
+			printf("greater than 5\n");
 		}
 		}
 	return (0);

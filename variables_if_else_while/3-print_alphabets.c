b5feba223bd23c37b5529946c8741d1522df648f
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-17 14:12:47 +0000

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'description for betty requirement'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = 'a';
+	char m = 'A';
+	char _a = '\n';
+
+	while (n <= 'z')
+	{
+	putchar(n);
+	n++;
+	}
+	while (m <= 'Z')
+        {
+        putchar(m);
+        m++;
+        }
+	putchar(_a);
+	return (0);
+}

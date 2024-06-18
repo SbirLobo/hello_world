7436655d81efc2b8ce6fdd207a34b3aa691e163a
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-18 11:38:07 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * Print alphabet
- *
- * Description: 'print alphabet'
- *
- * Return: no return
+ * print_alphabet - program for alphabet printing
+ * 
+ * Description: 'print alphabet in lowercase'
+ * 
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)

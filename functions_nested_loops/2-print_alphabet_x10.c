6c73a71895847b8efb72192999693b6d694644c6
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-18 11:52:29 +0000

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - program for alphabet printing
+ *
+ * Description: 'print alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char n = 'a';
+	char _a = '\n';
+	int i;
+
+	for (i = 0; i <10; i++)
+		n = 'a';
+	{
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	}
+	_putchar(_a);
+}

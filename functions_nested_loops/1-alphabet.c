c574fffefe5ea16636c7348e9d3612b005aea056
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-18 11:14:50 +0000

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char n = 'a';
+	char _a = '\n';
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar(_a);
+}

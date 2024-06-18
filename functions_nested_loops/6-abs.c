6215945f4c75fff5effb5ee87257ae9b803807e9
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-18 12:57:35 +0000

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - program for alphabet printing
+ *
+ * Description: 'print alphabet in lowercase'
+ *
+ * @a: entry
+ *
+ * Return: 1 if lowercase, 0 in other cases
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = -a;
+	}
+	return (a);
+}

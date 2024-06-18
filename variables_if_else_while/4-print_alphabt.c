9762ac5832b8a27e0776c5159adbe719c7aff783
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-18 07:23:29 +0000

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'description for betty requirement'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = 'a';
+	
+	char _a = '\n';
+
+	while (n <= 'z')
+	{
+	if (n == 'q' || n == 'e')
+		{
+		putchar(n);
+		n++;
+		}
+	}
+	putchar(_a);
+	return (0);
+}

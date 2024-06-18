df89f3f22af671652b7d82908a91f2271b3d636a
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-18 13:25:47 +0000

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - program for alphabet printing
+ *
+ * Description: 'print alphabet in lowercase'
+ *
+ * @a: entry
+ *
+ * Return: 1 if lowercase, 0 in other cases
+ */
+
+int print_last_digit(int a)
+{
+	int result;
+	if (a < 0)
+	{
+		a = -a;
+	}
+	result = a % 10;
+	_putchar('0' + result);
+	return (result);
+}

a6dcdd4b5fd6ce3bd7746e141be720d276751a36
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-20 12:43:27 +0000

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,8 +5,10 @@
  * Return: Always 0 (Success)
  * Description - Printing lines
  */
+
 int main(void)
-{
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }

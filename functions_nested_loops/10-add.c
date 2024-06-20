65cba0c4ef5d7eb03afe9f067eef7fef2fe6ae40
sbirlo <sebastien.birolleau@gmail.com>
2024-06-20 08:03:44 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - program for alphabet printing
+ *
+ * Description: 'print alphabet in lowercase'
+ *
+ * @num1: 1er int
+ *
+ * @num2: 2ème int
+ *
+ * Return: 1 if lowercase, 0 in other cases
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

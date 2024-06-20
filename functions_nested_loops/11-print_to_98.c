8970162e374d47ea40302239800c0f22d03514d7
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-20 10:08:00 +0000

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - program for alphabet printing
+ *
+ * Description: 'print alphabet in lowercase'
+ *
+ * @n: 1er int
+ *
+ * Return: 1 if lowercase, 0 in other cases
+ */
+
+void print_to_98(int n)
+{
+	int inc_or_dec = 1;
+
+	if (n > 98) 
+	{
+		inc_or_dec = -1;
+	}
+
+	while (n != 98)
+	{
+		printf("%d, ",n);
+		n += inc_or_dec;
+	}
+	printf("%d\n", 98);
+}

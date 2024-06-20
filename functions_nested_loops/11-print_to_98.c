403005a6dd917d6e7d1f80f6733415c382ef26bf
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-20 10:09:53 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 {
 	int inc_or_dec = 1;
 
-	if (n > 98) 
+	if (n > 98)
 	{
 		inc_or_dec = -1;
 	}
 
 	while (n != 98)
 	{
-		printf("%d, ",n);
+		printf("%d, ", n);
 		n += inc_or_dec;
 	}
 	printf("%d\n", 98);

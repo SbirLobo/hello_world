8f9fe050a5db4938464d155055fbd7cb8c3732b6
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-24 13:43:34 +0000

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_rev - print a reverse string
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	int numberOfChar = 0;
+
+	while (s[numberOfChar] != '\0')
+	{
+		numberOfChar++;
+	}
+
+	for (i = numberOfChar - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

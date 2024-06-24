eb829ef7d1a147bc7379c6bb3e974d6345e18ffe
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-24 08:33:22 +0000

    4

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9 whithout 2 and 4.
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char aa = '\n';
+
+	for (i = 0; i < 15; i++)
+	{
+		if (i > 9)
+		{
+			_putchar('0' + (i / 10));
+		}
+		_putchar('0' + i % 10);
+	}
+	_putchar(aa);
+}

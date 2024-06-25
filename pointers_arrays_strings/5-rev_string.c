31629a9e7c2e0a5cdc01eb29ec6ab4ad5910045f
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-25 13:36:49 +0000

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * new - create reverse
+ * rev_string - reverse string with pointer effect
+ * @n: length of string
+ * @s: char
+ */
+
+void new(int n, char *s)
+{
+	int i;
+	char tab[100];
+
+	for (i = 0; i < n; i++)
+	{
+		tab[i] = s[n - 1 - i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = *(tab + i);
+	}
+}
+
+void rev_string(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	new(n, s);
+}

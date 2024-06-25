649255f1daf491c36d95194de77f3f349fd035fe
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-25 13:57:15 +0000

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return length of string
+ * @s: string
+ * Return: int for response
+ */
+int _strlen(char *s)
+{
+	int numberOfChar = 0;
+
+	while (s[numberOfChar] != '\0')
+	{
+		numberOfChar++;
+	}
+	return (numberOfChar);
+}
+
+/**
+ * puts2 - create reverse
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int n = _strlen(str);
+
+	for (i = 0; i < n; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}

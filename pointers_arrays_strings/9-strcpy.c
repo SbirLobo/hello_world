dc5a9dd76e305adbc1ef2d8d8d2c2a11a29effc2
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-26 12:43:22 +0000

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,23 +2,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - return length of string
- * @s: string
- * Return: int for response
- */
-
-int _strlen(char *s)
-{
-	int numberOfChar = 0;
-
-	do {
-		printf("%c\n", s[numberOfChar]);
-		numberOfChar++;
-	} while (s[numberOfChar] != '\0');
-	return (numberOfChar);
-}
-
-/**
  * *_strcpy - copy a string pointed
  * @dest: array
  * @src: number of elements to print
@@ -29,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while  (src[i])
+	while  (src[i] != '\0')
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
-	*(dest + i + 1) = '\0';
+	dest[i + 1] = '\0';
 	return (dest);
 }

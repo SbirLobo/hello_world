2e7745667aa664f01f01a9bba27886f30e361418
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-26 07:48:56 +0000

    8

--- pointers_arrays_strings/8-main.c
+++ pointers_arrays_strings/8-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
     int array[5];
+    int array2[4] = { 999 };
 
     array[0] = 98;
     array[1] = 402;
@@ -15,5 +16,8 @@ int main(void)
     array[3] = 298;
     array[4] = -1024;
     print_array(array, 5);
+    print_array(array, 0);
+    print_array(array, -1);
+    print_array(array2, 2);
     return (0);
 }
--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 
 /**
+ * _strlen - return length of string
+ * @s: string
+ * Return: int for response
+ */
+int _arraylen(int *s)
+{
+	int numberOfChar = 0;
+
+	while (s[numberOfChar])
+	{
+		numberOfChar++;
+	}
+	return (numberOfChar);
+}
+
+/**
  * print_array - print n elements of an array
  * @a: array
  * @n: number of elements to print
@@ -9,7 +25,12 @@
 void print_array(int *a, int n)
 {
 	int i;
+	int lengthofarray = n;
+	lengthofarray = _arraylen(a);
 
+	n = (n < lengthofarray) ? n : lengthofarray;
+
+	printf("%d\t%d\n", lengthofarray, n);
 	for (i = 0; i < n; i++)
 	{
 		(i == n - 1) ? printf("%d\n", *(a + i)) : printf("%d, ", *(a + i));

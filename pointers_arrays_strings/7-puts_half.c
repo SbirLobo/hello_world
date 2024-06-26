b235a9faa240d9e865b467a2d9fe91bff3e9d57d
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-26 06:55:31 +0000

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -29,15 +29,12 @@ void puts_half(char *str)
 
 	if (n % 2 == 1)
 	{
-		
+		n++;
 	}
 
-	for (i = 0; i < n; i++)
+	for (i = n / 2; i < n; i++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

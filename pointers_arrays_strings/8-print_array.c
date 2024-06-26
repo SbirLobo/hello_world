b93d26840bc3881245d3e429133627b7fc7f5059
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-26 10:59:59 +0000

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,26 +2,11 @@
 #include <stdio.h>
 
 /**
- * _arraylen - return length of array
- * @s: array
- * Return: int for response
- */
-int _arraylen(int *s)
-{
-	int numberOfChar = 0;
-
-	while (s[numberOfChar])
-	{
-		numberOfChar++;
-	}
-	return (numberOfChar);
-}
-
-/**
  * print_array - print n elements of an array
  * @a: array
  * @n: number of elements to print
  */
+
 void print_array(int *a, int n)
 {
 	int i;

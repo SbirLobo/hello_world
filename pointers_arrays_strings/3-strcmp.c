3c27608b64eb4f5d4dc7b3a8a8b2aa806019f22a
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-27 12:02:07 +0000

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, length1 = 0, length2 = 0, result = 0;
+	int i = 0, length1 = 0, length2 = 0;
 
 	while (s1[length1] != '\0')
 		length1++;
 	while (s2[length2] != '\0')
 		length2++;
-	for (i = 0; i < length1 || i < length 2; i++)
-		result += s1[i] - s[2]
-	return (result);
+	while (s1[i] - s2[i] == 0)
+		i++;
+	return (i >= length1 && i >= length2) ? (0) : (s1[i] - s2[i]);
 }

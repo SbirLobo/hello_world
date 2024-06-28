6bec5a167c48d5147ae99ba5f37c4e5645cdf64d
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-28 13:56:39 +0000

    12

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - print integers
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	int i = 1;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (n % i < n)
+		i = i * 10;
+	
+	i = i / 10;
+	while (n > 0)
+	{
+		if (n % i == n)
+		{
+			_putchar('0');
+		}
+		else
+		{
+			_putchar((n - n % i) / i + '0');
+			n -= (n - n % i);
+		}
+		i = i / 10;
+	}
+}

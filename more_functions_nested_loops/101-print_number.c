957b2ebd87061d720b60ed550022a7bc80b469ee
sbirlobo <sebastien.birolleau@gmail.com>
2024-06-29 20:01:52 +0000

    12

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,7 @@
 
 void print_number(int n)
 {
-	unsigned int nn, i = 1;
+	unsigned int nn, i = 1000000000;
 
 	if (n < 0)
 	{
@@ -22,20 +22,12 @@ void print_number(int n)
 		_putchar('0');
 		return;
 	}
-	while (nn % (i * 10) < nn)
-		i = i * 10;
 
-	while (nn > 0)
+	while (i > 0)
 	{
-		if (nn % i == nn)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar((nn - nn % i) / i + '0');
-			nn -= (nn - nn % i);
-		}
-		i = i / 10;
+		if (nn > i || i == 1)
+			_putchar('0' + nn / i % 10);
+
+		i /= 10;
 	}
 }

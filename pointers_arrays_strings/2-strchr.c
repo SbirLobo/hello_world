4221e730ae7eaf2d5e64d68af823b9fb4137e14a
sbirlobo <sebastien.birolleau@gmail.com>
2024-07-01 08:55:52 +0000

    2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - locate character of string
+ * @s: string
+ * @c: character
+ * Return: pointer to character in string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return(&s[i]);
+		i++;
+	}
+
+	return (NULL);
+}
